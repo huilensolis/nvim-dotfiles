4f39cceca2164b7df3ae80212e5674123acac306
Huilensolis <xywvanz@gmail.com>
2024-10-11 16:35:33 -0300

    add some keymaps

--- lua/hvilen/keymap.lua
+++ lua/hvilen/keymap.lua
@@ -1,5 +1,41 @@
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+map("n", "<leader>pv", vim.cmd.Ex)
 
 -- nvim tree
-vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+map("n", "<leader>e", vim.cmd.NvimTreeToggle)
+
+-- window movement
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- Shift + Up/Down to move line up/down
+map('n', '<M-j>', 'yyddp')
+map('n', '<M-k>', 'yyddkP')
+
+-- Disable arrow keys
+map('', '<up>', '<nop>')
+map('', '<down>', '<nop>')
+map('', '<left>', '<nop>')
+map('', '<right>', '<nop>')
+
+-- vertical split
+map('n', '<C-s>', vim.cmd.vs)
+
+-- resize split windows
+-- map('n', '<S>c', function()
+--     vim.cmd("vertical resize +5")
+-- end)
+-- map('n', '<S>s', function()
+--     vim.cmd("vertical resize -5")
+-- end)
--- lua/hvilen/lazy/telescope.lua
+++ lua/hvilen/lazy/telescope.lua
@@ -2,6 +2,7 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = false,
     config = function()
         require("telescope").setup({})
 
